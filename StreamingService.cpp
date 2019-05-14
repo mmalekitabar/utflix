2ef076d6f00c99e521d8e27553e7310a50de79a4
mmalekitabar <mohammad.malekitabar@gmail.com>
2019-05-14 14:37:38 -0700

    fix first check input

--- StreamingService.cpp
+++ StreamingService.cpp
@@ -28,30 +28,43 @@ std::string StreamingService::get_input()
 void StreamingService::check_input(std::string input)
 {
 	std::string part1 = input.substr(0, input.find(' '));
-	std::cout << part1 << std::endl;
 	input.erase(0, input.find(' ') + 1);
-	std::string part2 = input.substr(0, input.find('?') - 1);
-	std::cout << part2 << std::endl;
+	std::string part2 = input.substr(0, input.find(' '));
 	if(part1 == "GET")
+	{
 		if(part2 != "followers" && part2 != "films" && part2 != "purchased" && part2 != "notifications" && part2 != "notifications read")
+		{
 			throw NotFound();
+		}
+	}
 	else if(part1 == "PUT")
+	{
 		if(part2 != "films")
+		{
 			throw NotFound();
+		}
+	}
 	else if(part1 == "DELETE")
+	{
 		if(part2 != "films" && part2 != "comments")
+		{
 			throw NotFound();
+		}
+	}
 	else if(part1 == "POST")
+	{
 		if(part2 != "signup" && part2 != "login" && part2 != "films" && part2 != "money" && part2 != "replies" && part2 != "followers" && part2 != "buy" && part2 != "rate" && part2 != "comments")
+		{
 			throw NotFound();
+		}
+	}
 	else
+	{
 		throw BadRequest();
-
-
-
+	}
 }
 
 void StreamingService::act_on_input(std::string input)
 {
-
+	std::cout << "act" << std::endl;
 }
